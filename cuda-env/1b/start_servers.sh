5c266e9430d6d512bf5469bb428cbdfe83e83d02
Xueyuan Jiang <alexxyjiang@gmail.com>
2019-11-26 11:25:21 +0800

    fix

--- cuda-env/1b/start_servers.sh
+++ cuda-env/1b/start_servers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /home/${USER}
-useradd -G rstudio-users -p `echo "${PASSWD}" | openssl passwd -1 -stdin` ${USER} && chown -R ${USER} /home/${USER} && chgrp -R ${USER} /home/${USER}
+useradd -G rstudio-users -p `echo "${PASSWD}" | openssl passwd -1 -stdin` ${USER} && chown ${USER} /home/${USER} && chgrp ${USER} /home/${USER}
 rstudio-server start
 su ${USER} -c "jupyter lab --no-browser --ip=\"0.0.0.0\" --notebook-dir=\"/home/${USER}\""
