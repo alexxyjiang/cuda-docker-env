8d7686b99cc7c7ea873ececdbfea40c33fab5211
Xueyuan Jiang <alexxyjiang@gmail.com>
2019-12-05 23:26:23 +0800

    fix

--- cuda-env/1b/start_servers.sh
+++ cuda-env/1b/start_servers.sh
@@ -2,4 +2,5 @@
 groupadd -g ${GID} ${USER}
 useradd -m -u ${UID} -g ${GID} -G rstudio-users -p `echo "${PASSWD}" | openssl passwd -1 -stdin` -s ${SHELL} ${USER}
 rstudio-server start
-chroot --userspec=${USER}:${USER} / ${SHELL} -c "env HOME=/home/${USER} jupyter lab --no-browser --ip=\"0.0.0.0\" --notebook-dir=\"/home/${USER}\""
+chroot --userspec=${USER}:${USER} / ${SHELL} -c "HOME=/home/${USER} && cd ~ && cp -r /.vim ~/.vim && cp /.vimrc ~/.vimrc"
+chroot --userspec=${USER}:${USER} / ${SHELL} -c "HOME=/home/${USER} jupyter lab --no-browser --ip=\"0.0.0.0\" --notebook-dir=\"/home/${USER}\""
