76b1ccd465f81b2919fb72ad414afddb6da7acd0
Xueyuan Jiang <alexxyjiang@gmail.com>
2019-12-06 10:05:08 +0800

    optimize vim config copies

--- cuda-env/1b/start_servers.sh
+++ cuda-env/1b/start_servers.sh
@@ -2,5 +2,5 @@
 groupadd -g ${GID} ${USER}
 useradd -m -u ${UID} -g ${GID} -G rstudio-users -p `echo "${PASSWD}" | openssl passwd -1 -stdin` -s ${SHELL} ${USER}
 rstudio-server start
-chroot --userspec=${USER}:${USER} / ${SHELL} -c "HOME=/home/${USER} && cd ~ && cp -r /.vim ~/.vim && cp /.vimrc ~/.vimrc"
+chroot --userspec=${USER}:${USER} / ${SHELL} -c "HOME=/home/${USER} && cd ~ && cp /vim.tar.gz ./ && tar -zxf vim.tar.gz"
 chroot --userspec=${USER}:${USER} / ${SHELL} -c "HOME=/home/${USER} jupyter lab --no-browser --ip=\"0.0.0.0\" --notebook-dir=\"/home/${USER}\""
