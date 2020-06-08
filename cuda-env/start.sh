e7dd03180a3c50b47367ac92e799f290ea27288f
Xueyuan Jiang <alexxyjiang@gmail.com>
2020-06-08 23:11:41 +0800

    support graph ploting in octave

--- cuda-env/start.sh
+++ cuda-env/start.sh
@@ -2,5 +2,5 @@ if [ $# -lt 1 ]; then
   echo "usage: sh ${0} [work-path]"
   exit 1
 else
-  docker run -d --userns=host --network=host --name deep-learning --rm -e USER=$(id -un) -e PASSWD="M00n" -e UID=$(id -u) -e GID=$(id -g) -v ${1}:/home/$(id -un)/ alexxyjiang/cuda-env:deep-learning
+  docker run -d --userns=host --network=host --gpus all --name deep-learning --rm -e DISPLAY=${DISPLAY} -e USER=$(id -un) -e PASSWD="M00n" -e UID=$(id -u) -e GID=$(id -g) -v /tmp/.X11-unix:/tmp/.X11-unix -v ${1}:/home/$(id -un)/ alexxyjiang/cuda-env:deep-learning
 fi
