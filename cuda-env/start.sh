55de7c026362d1063b11c24b4e87c0c01104fb3c
Xueyuan Jiang <alexxyjiang@gmail.com>
2020-07-12 15:28:15 +0800

    fix env XDG_RUNTIME_DIR

--- cuda-env/start.sh
+++ cuda-env/start.sh
@@ -2,5 +2,5 @@ if [ $# -lt 1 ]; then
   echo "usage: sh ${0} [work-path]"
   exit 1
 else
-  docker run -d --userns=host --network=host --gpus all --name deep-learning --rm -e DISPLAY=${DISPLAY} -e USER=$(id -un) -e PASSWD="M00n" -e UID=$(id -u) -e GID=$(id -g) -v /tmp/.X11-unix:/tmp/.X11-unix -v ${1}:/home/$(id -un)/ alexxyjiang/cuda-env:deep-learning
+  docker run -d --rm --name deep-learning --gpus all --network=host --userns=host -e DISPLAY=${DISPLAY} -e XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR} -e USER=$(id -un) -e PASSWD="M00n" -e UID=$(id -u) -e GID=$(id -g) -v /tmp/.X11-unix:/tmp/.X11-unix -v ${XDG_RUNTIME_DIR}:${XDG_RUNTIME_DIR} -v ${1}:/home/$(id -un)/ alexxyjiang/cuda-env:deep-learning
 fi
